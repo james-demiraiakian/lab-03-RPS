b2e38d4623fca019eba088dd96b20cf31f05b2fc
James Demiraiakian <james.armen@gmail.com>
2021-10-06 16:41:28 -0700

    imported functions and declared variables, partial event listener on play button

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-import { getRandomThrow } from "./get-random-throw.js";
+import { getRandomThrow, didUserWin } from './get-random-throw.js';
+
+
+const play = document.getElementById('play');
+const reset = document.getElementById('reset');
+const result = document.getElementById('result');
+const wins = document.getElementById('wins');
+const losses = document.getElementById('losses');
+const draws = document.getElementById('draws');
+const resets = document.getElementById('resets');
+const error = document.getElementById('error');
+
+let winCount = 0;
+let lossCount = 0;
+let drawCount = 0;
+let resetCount = 0;
 
 console.log(getRandomThrow());
+//console.log(playerChoice);
+
+play.addEventListener('click', () => {
+    const player = document.querySelector('input[type=radio]:checked');
+
+    if (!player) {
+        return error.classList.remove('hidden');
+    }
+    error.classList.add('hidden');
+
+    const playerChoice = player.value;
+    const computerChoice = getRandomThrow();
+
+    if (didUserWin(playerChoice, computerChoice) === 'win') {
+        wins++;
+        
+    }
+
+});
--- get-random-throw.js
+++ get-random-throw.js
@@ -1,17 +1,8 @@
 export function getRandomThrow() {
-    return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)]
+    return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
 }
 
 export function didUserWin(player, computer) {
-
-// possible outcomes: 'win', 'lose', 'draw'
-
-// if the userThrow === computerThrow return 'draw'
-// if the user throws rock && computer throws scissors, return 'win'
-// else if the user throws paper && computer throws rock, return 'win'
-// else if the user throws scissors && computer throws paper, return 'win'
-// else return 'lose'
-
     if (player === computer) {
         return 'draw';
     } else
@@ -24,9 +15,6 @@ export function didUserWin(player, computer) {
     if (player === 'scissors' && computer === 'paper') {
         return 'win';
     } else {
-        return 'draw';
+        return 'lose';
     }
-        
-
-
 }
